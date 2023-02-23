75631e4f97be8a21d107683a7d64e1bf916c5b4f
FernandoRodero <78755179+FernandoRodero@users.noreply.github.com>
2023-02-23 22:13:55 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,13 @@
-for (let index = 0; index < 2; index++) {
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-}
-basic.showString("Hola!")
-basic.showString("La temperatura es:")
+maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 basic.showString("" + (input.temperature()))
 basic.showString("ºC")
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 12) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 15) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     } else {
-        while (maqueen.Ultrasonic(PingUnit.Centimeters) <= 12) {
+        while (maqueen.Ultrasonic(PingUnit.Centimeters) <= 15) {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 80)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 80)
         }
